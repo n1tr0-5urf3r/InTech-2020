924365d4fad11a9d3d8c27dbaabe09442d3e16a7
n1tr0-5urf3r <fabi@ihlecloud.de>
2020-05-25 18:33:29 +0200

    Added difficulty selector

--- uebung05/snake.js
+++ uebung05/snake.js
@@ -9,6 +9,8 @@ var snake = [{ x: 3, y: 2 }, { x: 4, y: 2 }, { x: 5, y: 2 }];
 var fruit = { x: 5, y: 5 };
 var currentDirection = toLeft;
 var canvas = document.getElementById('drawing');
+var speed = 150;
+var started = false;
 
 function drawRect(x, y, lineColor, fillColor) {
     if (canvas.getContext) {
@@ -96,6 +98,7 @@ function moveSnake(snake, directionVector) {
     function mod(n, m) {
         return ((n % m) + m) % m;
     }
+
     var tail = snake[snake.length - 1];
     snake.pop();
     var newHeadX = mod(snake[0].x + directionVector.x, 20);
@@ -105,61 +108,80 @@ function moveSnake(snake, directionVector) {
     return tail;
 }
 
-// Game- and Animationloop
-var intervalID = setInterval(function () {
 
-    /**
-     * If two different arrow keys are pressed at the same time in the same iteration, it might happen that the snake collides with itself.
-     * Like currently moving down, pressing right and top at the same time, results in the snake moving up and dying while that should not be allowed.
-     * Thus this flag will prevent this and only count the first key event
-     *  */
-    var keyPressed = false;
+// Wait for start ....
+document.getElementById("start").addEventListener("click", setDiff);
+drawFruit(fruit);
+drawSnake(snake);
+if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    ctx.font = "19px Arial";
+    ctx.fillStyle = "red"
+    ctx.fillText("Please select difficulty and press start", 30, 300);
+}
 
-    // Listener for user input
-    document.body.addEventListener('keydown', function (event) {
-        if (event.key == "ArrowUp" && currentDirection != toBottom && !keyPressed) {
-            currentDirection = toUp;
-            keyPressed = true;
-        }
-        if (event.key == "ArrowDown" && currentDirection != toUp && !keyPressed) {
-            currentDirection = toBottom;
-            keyPressed = true;
-        }
-        if (event.key == "ArrowLeft" && currentDirection != toRight && !keyPressed) {
-            currentDirection = toLeft;
-            keyPressed = true;
-        }
-        if (event.key == "ArrowRight" && currentDirection != toLeft && !keyPressed) {
-            currentDirection = toRight;
-            keyPressed = true;
-        }
-    });
+function setDiff(){
+    speed = parseInt(document.getElementById("options").value);
+    console.log(speed);
+    started = true;
 
-    var tail = moveSnake(snake, currentDirection);
-    if (canvas.getContext) {
-        var ctx = canvas.getContext('2d');
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-    }
-    if (fruitCollidesWithSnake(fruit, snake)) {
-        // Make snake longer
-        snake.push(tail);
+    // Game- and Animationloop
+    var intervalID = setInterval(function () {
 
-        // Finished the game, gg! Also prevents non terminating loops of trying to generate new fruits
-        if (snake.length == 400) {
+        /**
+         * If two different arrow keys are pressed at the same time in the same iteration, it might happen that the snake collides with itself.
+         * Like currently moving down, pressing right and top at the same time, results in the snake moving up and dying while that should not be allowed.
+         * Thus this flag will prevent this and only count the first key event
+         *  */
+        var keyPressed = false;
+
+        // Listener for user input
+        document.body.addEventListener('keydown', function (event) {
+            if (event.key == "ArrowUp" && currentDirection != toBottom && !keyPressed) {
+                currentDirection = toUp;
+                keyPressed = true;
+            }
+            if (event.key == "ArrowDown" && currentDirection != toUp && !keyPressed) {
+                currentDirection = toBottom;
+                keyPressed = true;
+            }
+            if (event.key == "ArrowLeft" && currentDirection != toRight && !keyPressed) {
+                currentDirection = toLeft;
+                keyPressed = true;
+            }
+            if (event.key == "ArrowRight" && currentDirection != toLeft && !keyPressed) {
+                currentDirection = toRight;
+                keyPressed = true;
+            }
+        });
+
+        var tail = moveSnake(snake, currentDirection);
+        if (canvas.getContext) {
+            var ctx = canvas.getContext('2d');
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+        }
+        if (fruitCollidesWithSnake(fruit, snake)) {
+            // Make snake longer
+            snake.push(tail);
+
+            // Finished the game, gg! Also prevents non terminating loops of trying to generate new fruits
+            if (snake.length == 400) {
+                drawGameOver(snake.length - 3);
+                clearInterval(intervalID);
+            }
+
+            // Generate new fruit
+            fruit = randomCoordinatesOutsideSnake(snake);
+        }
+        drawFruit(fruit);
+        drawSnake(snake);
+
+        // Game over, stop intervalID
+        if (snakeHeadCollidesWithSnake(snake)) {
+            drawRect(snake[0].x * 20, snake[0].y * 20, "#D3D3D3", "#FF0000")
             drawGameOver(snake.length - 3);
             clearInterval(intervalID);
         }
+    }, speed);
 
-        // Generate new fruit
-        fruit = randomCoordinatesOutsideSnake(snake);
-    }
-    drawFruit(fruit);
-    drawSnake(snake);
-
-    // Game over, stop intervalID
-    if (snakeHeadCollidesWithSnake(snake)) {
-        drawRect(snake[0].x * 20, snake[0].y * 20, "#D3D3D3", "#FF0000")
-        drawGameOver(snake.length - 3);
-        clearInterval(intervalID);
-    }
-}, 150);
+}
