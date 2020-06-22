b78d47a7b6404bb6febb0279274d3b104647cee8
n1tr0-5urf3r <fabi@ihlecloud.de>
2020-06-22 16:43:33 +0200

    test

--- uebung8/hangman-guess.php
+++ uebung8/hangman-guess.php
@@ -5,7 +5,10 @@ session_start();
 
 if( isset($_POST['letter']) )
 {
+    print($_POST['letter']);
     guessLetter($letter);
+}else {
+    print($_POST['letter']);
 }
 header("Location: hangman.php");
 
