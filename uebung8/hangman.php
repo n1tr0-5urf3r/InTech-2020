c14b54330008effe610dc753f2e700b1cb3d4fc4
n1tr0-5urf3r <fabi@ihlecloud.de>
2020-06-22 16:19:48 +0200

    Task 4

---
+++ uebung8/hangman.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+    if (!isset($_SESSION['toGuess'])) {
+        header("Location: 'hangman-init.php'");
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hangman Wörter</title>
+</head>
+<body>
+    <h1>Wörter raten</h1>
+
+    <?php
+        // Display mask
+        print("<p>");
+        foreach ($_SESSION["mask"] as $char){
+            print("{$char} ");
+        }
+        print("</p>");
+    ?>
+    <form>
+        <?php
+        $allLetters = array("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z");
+        $notUsed = array_diff($allLetters, $_SESSION["guessedLetters"]);
+        foreach ($notUsed as $letter){
+            print("<input type='submit' value='{$letter}'>");
+        }
+        ?>
+    </form>
+
+
+</body>
+</html>
