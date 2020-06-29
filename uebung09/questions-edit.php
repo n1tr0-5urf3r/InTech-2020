beb8b9fcf601c3305c740703b09bb23e15831be6
n1tr0-5urf3r <fabi@ihlecloud.de>
2020-06-29 13:11:48 +0200

    .

--- uebung09/questions-edit.php
+++ uebung09/questions-edit.php
@@ -8,7 +8,6 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $q =  $db->getQuestion($id);
 
         $question = $q['question'];
-        $id = $q['id'];
         $answer0 = $q['answer0'];
         $answer1 = $q['answer1'];
         $answer2 = $q['answer2'];
@@ -30,16 +29,16 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     <form action="questions-update.php" method="post">
         <input type="hidden" id="questionId" name="questionId" value=<?php echo '$id' ?>>
         <h3>Frage:</h3>
-        <textarea cols="40" rows="5" name="question" value=<?php echo '$question' ?> required></textarea><br>
+        <textarea cols="40" rows="5" name="question" value=<?php echo $question ?> required></textarea><br>
         <h3>Antwort 0</h3>
         <input type=radio name="solution" value="0" required>
-        <input type=text name="answer0" value=<?php echo '$answer0' ?> required></br>
+        <input type=text name="answer0" value=<?php echo $answer0 ?> required></br>
         <h3>Antwort 1</h3>
         <input type=radio name="solution" value="1" required>
-        <input type=text name="answer1" value=<?php echo '$answer1' ?> required></br>
+        <input type=text name="answer1" value=<?php echo $answer1 ?> required></br>
         <h3>Antwort 2</h3>
         <input type=radio name="solution" value="2" required>
-        <input type=text name="answer2" value=<?php echo '$answer2' ?> required></br>
+        <input type=text name="answer2" value=<?php echo $answer2 ?> required></br>
 
         <input type=submit name="store" value="Frage aktualisieren">
 
