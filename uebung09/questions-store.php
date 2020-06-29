5974b969890648db995f9bde9cdcd08f67ee47e7
n1tr0-5urf3r <fabi@ihlecloud.de>
2020-06-29 11:38:32 +0200

    Actually insert question before redirect

--- uebung09/questions-store.php
+++ uebung09/questions-store.php
@@ -8,9 +8,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $answer1 = $_POST['answer1'];
     $answer2 = $_POST['answer2'];
     $solution = $_POST['solution'];
-    header("Location: questions-show.php");
-
 
     $db = new Database();
     $db->insertQuestion($question, $answer0, $answer1, $answer2, $solution);
+
+    header("Location: questions-show.php");
 }
