287c8b9eddd101f27bb5d0c6938e60fe6b1c3ed3
n1tr0-5urf3r <fabi@ihlecloud.de>
2020-06-29 14:07:45 +0200

    default radio buttons

--- uebung09/questions-edit.php
+++ uebung09/questions-edit.php
@@ -7,11 +7,27 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
     $db = new Database();
     $q =  $db->getQuestion($id);
 
-        $question = $q['question'];
-        $answer0 = $q['answer0'];
-        $answer1 = $q['answer1'];
-        $answer2 = $q['answer2'];
-        $solution = $q['solution'];
+    $question = $q['question'];
+    $answer0 = $q['answer0'];
+    $answer1 = $q['answer1'];
+    $answer2 = $q['answer2'];
+    $solution = $q['solution'];
+
+    $s0 = '';
+    $s1 = '';
+    $s2 = '';
+
+    switch ($solution) {
+        case 0:
+            $s0 = 'checked';
+            break;
+        case 1:
+            $s1 = 'checked';
+            break;
+        case 2:
+            $s2 = 'checked';
+            break;
+    }
 }
 ?>
 
@@ -31,13 +47,13 @@ if ($_SERVER["REQUEST_METHOD"] == "GET") {
         <h3>Frage:</h3>
         <textarea cols="40" rows="5" name="question" required><?php echo $question ?></textarea><br>
         <h3>Antwort 0</h3>
-        <input type=radio name="solution" value="0" required>
+        <input type=radio name="solution" value="0" required checked=<?php echo $s0?>>
         <input type=text name="answer0" value="<?php echo $answer0 ?>" required></br>
         <h3>Antwort 1</h3>
-        <input type=radio name="solution" value="1" required>
+        <input type=radio name="solution" value="1" required checked=<?php echo $s1?>>
         <input type=text name="answer1" value="<?php echo $answer1 ?>" required></br>
         <h3>Antwort 2</h3>
-        <input type=radio name="solution" value="2" required>
+        <input type=radio name="solution" value="2" required checked=<?php echo $s2?>>
         <input type=text name="answer2" value="<?php echo $answer2 ?>" required></br>
 
         <input type=submit name="store" value="Frage aktualisieren">
